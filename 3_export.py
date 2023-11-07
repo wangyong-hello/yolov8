036191fdd7f606b2dddcf50ed62791eb5f0d567a
wangyong <wangyong_mails@163.com>
2023-11-07 13:39:04 +0800

    双

--- 3_export.py
+++ 3_export.py
@@ -7,5 +7,6 @@
 # model = YOLO("./runs/detect/train/weights/last.pt ")  # load a pretrained YOLOv8n model
 # model.export(format="onnx")  # export the model to ONNX format
 
+
 for i,name in enumerate(['LA', 'PC', 'SA', 'RA', 'SLA', 'SRA', 'MAR', 'CL', 'STA', 'NTA', 'LRA', 'MAL', 'SLR', 'other', 'TA', 'LTA']):
     print(f"{i}: {name}")
