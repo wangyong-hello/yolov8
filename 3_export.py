37d07122f208d690404e5d0418d75604dea277d6
wangyong <wangyong_mails@163.com>
2024-01-04 15:14:54 +0800

    update

--- 3_export.py
+++ 3_export.py
@@ -4,8 +4,13 @@
 # python
 from ultralytics import YOLO
 
-model = YOLO("/home/xnwu/wangyong/Code/Yolov8/runs/detect/yolov8n_train_dataset9_val3_norect_no_fliplr_no_scale_crop/weights/best.pt")  # load a pretrained YOLOv8n model
-model.export(format="onnx",imgsz=[192,320])  # export the model to ONNX format
+# model = YOLO("/home/xnwu/wangyong/Code/Yolov8/runs/detect/yolov8n_train_dataset9_val3_norect_no_fliplr_no_scale_crop/weights/best.pt")  # load a pretrained YOLOv8n model
+# model.export(format="onnx",imgsz=[192,320])  # export the model to ONNX format
+
+model = YOLO("/home/xnwu/wangyong/Code/Yolov8/official_weights/yolov8s.pt")  # load a pretrained YOLOv8n model
+model.export(format="onnx",dynamic=True,simplify=True)  # export the model to ONNX format
+
+
 
 
 # for i,name in enumerate(['LA', 'PC', 'SA', 'RA', 'SLA', 'SRA', 'MAR', 'CL', 'STA', 'NTA', 'LRA', 'MAL', 'SLR', 'other', 'TA', 'LTA']):
